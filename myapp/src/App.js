377483542bb35fb4b5b0d5c9bf1eedf808a58417
Sandra Marquez <sandra@Sandras-MacBook-Air.local>
2017-12-09 13:14:34 -0600

    update

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import card from "./card.json";
 import './App.css';
 
+
 class App extends Component {
+  // Setting this.state.card to the cards json array
+  state = {
+    card,
+    count: 0
+  };
+
+
+  handleIncrement = id => {
+       // We always use the setState method to update a component's state
+    this.setState({ count: this.state.count + 1 });
+  };
+
+
+
+  // Map over this.state.card and render a Card component for each card object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+        <Title>Starwars</Title>
+        {this.state.card.map(card => (
+          <Card
+            removeCard={this.removeCard}
+            id={card.id}
+            key={card.id}
+            name={card.name}
+            image={card.image}
+            occupation={card.occupation}
+            location={card.location}
+          />
+        ))}
+      </Wrapper>
     );
   }
-}
+} 
+
+
 
 export default App;
